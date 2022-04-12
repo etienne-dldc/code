88204fe2bd00e33258397654b9cc78328d07f65f
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-04-12 15:16:07 +0200

    Add monaco & prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,62 @@
+import * as monaco from "monaco-editor";
+import prettier from "prettier/standalone";
 import "./style.css";
+
+console.log(prettier);
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const modelUri = monaco.Uri.file("file.tsx");
+
+monaco.languages.typescript.typescriptDefaults.addExtraLib(
+  `declare module "react/jsx-runtime"`,
+  "global.d.ts"
+);
+
+const STORAGE_KEY = "monaco-editor-value";
+
+const initialContent = localStorage.getItem(STORAGE_KEY) ?? "";
+
+const codeModel = monaco.editor.createModel(
+  initialContent,
+  "typescript",
+  modelUri
+);
+
+monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+  jsx: monaco.languages.typescript.JsxEmit.ReactJSX,
+});
+
+monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+  noSemanticValidation: false,
+  noSyntaxValidation: false,
+});
+
+const editor = monaco.editor.create(app, {
+  theme: "vs-dark",
+  language: "typescript",
+  formatOnType: false,
+  fontSize: 36,
+  tabSize: 2,
+  padding: { top: 30, bottom: 30 },
+  lineNumbersMinChars: 3,
+  minimap: {
+    enabled: true,
+  },
+  scrollbar: {
+    useShadows: false,
+  },
+  mouseWheelZoom: true,
+});
+
+editor.setModel(codeModel);
+
+codeModel.onDidChangeContent(() => {
+  const newValue = editor.getValue();
+  console.log(newValue);
+  localStorage.setItem(STORAGE_KEY, newValue);
+});
+
+window.addEventListener("resize", function () {
+  editor.layout();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [monacoEditorPlugin()],
+});
